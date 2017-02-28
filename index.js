292b9eec664a126e0ad658d6d570588fb4213ffa
Travis CI <fl@nyggen.com>
2017-02-28 16:47:35 +0000

    Deploy to GitHub Pages: f2f05671bb1897d7647cf9a358e31cd0423166fb

---
+++ index.js
@@ -0,0 +1,150 @@
+var app =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony export (immutable) */ __webpack_exports__["UpdateLogo"] = UpdateLogo;
+var fonts = [
+	{name : "Roboto"},
+	{name : "Open Sans"},
+	{name : "Josefin Slab"},
+	{name : "Arvo"},
+	{name : "Lato"},
+	{name : "Abril Fatface"},
+	{name : "Ubuntu"},
+	{name : "PT Sans"},
+	{name : "Old Standard TT"},
+	{name : "Droid Sans"},
+	{name : "Sansita"}
+];
+
+function hexToColor(hex) {	
+	return "#" + ("000000" + hex.toString(16)).substr(-6);
+}
+
+function UpdateLogo(word) {
+	var parentDiv = document.getElementById("text");
+
+	// Get a new bg color
+	var bgColor = Math.floor(Math.random() * 0xFFFFFF);
+	parentDiv.style.backgroundColor = hexToColor(bgColor);
+
+	// TODO: this is a horrible algorithm for choosing an oposite color
+	// find somehting better: http://softwareengineering.stackexchange.com/questions/44929/color-schemes-generation-theory-and-algorithms
+	// http://serennu.com/colour/rgbtohsl.php
+	var value = ((bgColor & 0x0F) << 4) | ((bgColor & 0xF0) >> 4);
+	value = ((value & 0x33) << 2) | ((value & 0xCC) >> 2);
+
+	var choosenFont = Math.floor(Math.random()*fonts.length);
+
+	if(typeof fonts[choosenFont].loaded == 'undefined') {
+		var link = document.createElement("link");
+		link.href="https://fonts.googleapis.com/css?family=" + fonts[choosenFont].name.split(' ').join('+');
+		link.rel="stylesheet"
+			document.body.appendChild(link);
+		fonts[choosenFont].loaded = true;
+	}
+
+	var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+	var textSvg = document.createElementNS(svg.namespaceURI, "text");
+	textSvg.setAttributeNS(null, "x", 0);
+	textSvg.setAttributeNS(null, "y", 25);
+	textSvg.setAttributeNS(null, "fill", hexToColor(value));
+	textSvg.setAttributeNS(null, "font-family", fonts[choosenFont].name);
+	svg.appendChild(textSvg);
+	textSvg.appendChild(document.createTextNode(word.Word));
+
+	parentDiv.innerHTML="";
+	parentDiv.appendChild(svg);
+}
+
+window.onload = function() {
+	window.onkeydown = function(e){
+		if(e.keyCode == 32){
+			var s=document.createElement("script");
+			s.type="text/javascript";
+			s.src="http://www.setgetgo.com/randomword/get.php?callback=app.UpdateLogo";
+			document.body.appendChild(s);
+		}
+		
+		// Prevent scrolling
+		if(e.keyCode == 32 || e.which == 32) {
+			e.preventDefault();
+			return 0;
+		}
+	}
+};
+
+
+/***/ })
+/******/ ]);
